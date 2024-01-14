db754ba55ee47fa4b488f1c8c4080883e35c913e
Jin Kim <kimjin1988@gmail.com>
2024-01-14 12:21:33 +0900

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+export default function() {
+    return (
+        <>
+            <div className="bg-stone-300 text-white text-[20px] bottom-0">
+                <div className="container">
+                    <h2>끝까지 봐주셔서 감사합니다 :)</h2>
+                    <p>Email: wlsdlflgy@naver.com</p>
+                    <p></p>
+                </div>
+            <footer className="bg-stone-300 text-[16px] text-white box-border w-full h-auto xl:text-center">
+                <p>&copy; 2023 KIM JIN. All rights reserved.</p>
+            </footer>
+            </div>
+        </>
+    )
+};
